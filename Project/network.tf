313964c91117cbececfd622914d328eff3402ada
Or Zaguri <Ozzy045@gmail.com>
2021-10-16 08:59:26 +0300

    Add NSG and associate to subnets

--- Project/network.tf
+++ Project/network.tf
@@ -21,3 +21,95 @@ resource "azurerm_subnet" "privatesubnet" {
     address_prefixes     = ["11.0.2.0/24"]
 
 }
+
+# Create Network Security Group and rule
+resource "azurerm_network_security_group" "public_nsg" {
+    name                = "public-ngs"
+    location            = azurerm_resource_group.rg.location
+    resource_group_name = azurerm_resource_group.rg.name
+
+    security_rule {
+        name                       = "SSH"
+        priority                   = 100
+        direction                  = "Inbound"
+        access                     = "Allow"
+        protocol                   = "Tcp"
+        source_port_range          = "*"
+        destination_port_range     = "22"
+        source_address_prefix      = "*"
+        destination_address_prefix = "*"
+    }
+    security_rule {
+        name                       = "InBound-8080"
+        priority                   = 101
+        direction                  = "Inbound"
+        access                     = "Allow"
+        protocol                   = "*"
+        source_port_range          = "*"
+        destination_port_range     = "8080"
+        source_address_prefix      = "*"
+        destination_address_prefix = "*"
+    }
+    security_rule {
+        name                       = "OutBound-8080"
+        priority                   = 102
+        direction                  = "Outbound"
+        access                     = "Allow"
+        protocol                   = "*"
+        source_port_range          = "*"
+        destination_port_range     = "8080"
+        source_address_prefix      = "*"
+        destination_address_prefix = "*"
+    }
+
+    depends_on = [
+        azurerm_resource_group.rg,
+    ]
+}
+
+resource "azurerm_network_security_group" "private_nsg" {
+  name                = "private-nsg"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  security_rule {
+    name                       = "Rule-5432"
+    priority                   = 110
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "5432"
+    destination_port_range     = "5432"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "Rule-22"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "*"
+    source_port_range          = "22"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+
+    
+  }
+
+  depends_on = [
+    azurerm_resource_group.rg,
+  ]
+}
+
+# Connect the security group to the subnets
+resource "azurerm_subnet_network_security_group_association" "public_nsg_assoc" {
+    subnet_id      = azurerm_subnet.publicsubnet.id
+    network_security_group_id = azurerm_network_security_group.public_nsg.id
+}
+
+resource "azurerm_subnet_network_security_group_association" "private_nsg_assoc" {
+    subnet_id      = azurerm_subnet.privatesubnet.id
+    network_security_group_id = azurerm_network_security_group.private_nsg.id
+}
