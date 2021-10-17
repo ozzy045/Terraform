453c0976f4d66449805842f8dc43f1239ccd246f
ozzy045 <77283071+ozzy045@users.noreply.github.com>
2021-10-17 19:00:37 +0300

    Delete backend.tf

--- Project/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-    backend "azurerm" {
-        resource_group_name  = "terraform-project"
-        storage_account_name = "tfstatezdk5m"
-        container_name       = "tfstate"
-        key                  = "terraform.tfstate"
-    }
-
-}
